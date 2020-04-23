298128408a70b69fd80d4e08b1cbab88a04c6a47
master <ulassoyubey@hotmail.com>
2020-04-23 16:47:37 +0300

    Proje dosyası ekle.

---
+++ RanoBdd/SpecFlowFeature1.feature
@@ -0,0 +1,10 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Scenario: Login eksisozluk
+	Given I have navigated eksisozluk login page
+	And I have entered my credentials
+	When I press login
+	Then I need to be succesfully logged
---
+++ RanoBdd/SpecFlowFeature1Steps.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace RanoBdd
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "www.eksisozluk.com");
+
+        }
+        [Given(@"I have navigated eksisozluk login page")]
+        public void GivenIHaveNavigatedEksisozlukLoginPage()
+        {
+            Ranorex.WebDocument webDocument = "/dom[@domain='eksisozluk.com']";
+            Ranorex.ATag login = webDocument.FindSingle(".//a[#'top-login-link']");
+            login.Click();
+        }
+        
+        [Given(@"I have entered my credentials")]
+        public void GivenIHaveEnteredMyCredentials()
+        {
+            Ranorex.WebDocument webDocument = "/dom[@domain='eksisozluk.com']";
+            Ranorex.InputTag userName = webDocument.FindSingle(".///input[#'username']");
+            userName.PressKeys("ulassoyubey");
+            Ranorex.InputTag password = webDocument.FindSingle(".///input[#'password']");
+            password.PressKeys("1234567");
+
+        }
+        
+        [When(@"I press login")]
+        public void WhenIPressLogin()
+        {
+            Ranorex.WebDocument webDocument = "/dom[@domain='eksisozluk.com']";
+            Ranorex.ButtonTag login = webDocument.FindSingle(".//div[#'login-form-container']/form[@action='https://eksisozluk.com/giris']//button[@innertext='giriş yapmaya çabala']");
+            login.Click();
+        }
+        
+        [Then(@"I need to be succesfully logged")]
+        public void ThenINeedToBeSuccesfullyLogged()
+        {
+            Ranorex.WebDocument webDocument = "/dom[@domain='eksisozluk.com']";
+            Ranorex.SpanTag errorMessage = webDocument.FindSingle(".///div[#'login-form-container']/?/?/span[]");
+        }
+    }
+}
